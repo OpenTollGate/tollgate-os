a78d5057f3c7226bb119f22ae38a709d37c37dab
origami74@gmail.com <origami74@gmail.com>
2025-03-25 18:25:49 +0000

    Fix uci update syntax

--- files/etc/uci-defaults/firstboot-script.sh
+++ files/etc/uci-defaults/firstboot-script.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # This script configures wireless interfaces/devices on first boot
 
-uci set wireless.@wifi-iface.default_radio0.name='tollgate_2g_open'
-uci set wireless.@wifi-iface.default_radio0.ssid='TollGate - Setup'
-uci set wireless.@wifi-iface.default_radio0.encryption='none'
+uci set wireless.default_radio0.name='tollgate_2g_open'
+uci set wireless.default_radio0.ssid='TollGate - Setup'
+uci set wireless.default_radio0.encryption='none'
 
-uci set wireless.@wifi-iface.default_radio1.name='tollgate_5g_open'
-uci set wireless.@wifi-iface.default_radio1.ssid='TollGate - Setup'
-uci set wireless.@wifi-iface.default_radio1.encryption='none'
+uci set wireless.default_radio1.name='tollgate_5g_open'
+uci set wireless.default_radio1.ssid='TollGate - Setup'
+uci set wireless.default_radio1.encryption='none'
 
 # Enable wireless interfaces
-uci set wireless.@wifi-device.radio0.disabled='0'
-uci set wireless.@wifi-device.radio1.disabled='0'
+uci set wireless.radio0.disabled='0'
+uci set wireless.radio1.disabled='0'
 
 # Commit the changes to the wireless configuration
 uci commit wireless
