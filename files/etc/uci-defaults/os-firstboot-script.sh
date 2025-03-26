46ab9cd31783cf226a11b6529ebeec8092bf31bf
Arjen <18398758+Origami74@users.noreply.github.com>
2025-03-26 18:17:43 +0100

    V0.0.1 (#8)
    * add captive portal page
    
    * set default wifi
    
    * update existing ifaces and enable radio's
    
    * cleanup
    
    * fix updating
    
    * Fix uci update syntax
    
    * rename script file + make executable
    
    * add nodogsplash config
    
    * add comments to nodogsplash config + add flashing os script
    
    * modt -> banner
    
    * fix dns defaulting to localhost by setting cloudflare as default
    
    ---------
    
    Co-authored-by: origami74@gmail.com <origami74@gmail.com>

---
+++ files/etc/uci-defaults/os-firstboot-script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# This script configures wireless interfaces/devices on first boot
+
+uci set wireless.default_radio0.name='tollgate_2g_open'
+uci set wireless.default_radio0.ssid='TollGate - Setup'
+uci set wireless.default_radio0.encryption='none'
+
+uci set wireless.default_radio1.name='tollgate_5g_open'
+uci set wireless.default_radio1.ssid='TollGate - Setup'
+uci set wireless.default_radio1.encryption='none'
+
+# Enable wireless interfaces
+uci set wireless.radio0.disabled='0'
+uci set wireless.radio1.disabled='0'
+
+# Commit the changes to the wireless configuration
+uci commit wireless
+
+# Additional commands can be added here
+echo "Wireless radios enabled, interfaces for 'TollGate - Setup' configured and applied on first boot."
+
+uci set network.lan.dns='1.1.1.1' # CloudFlare DNS (Change to more privacy focussed dns??)
+uci commit network
